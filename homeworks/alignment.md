7f65d933cbe0f4e84d63c077f4def2a22a34a482
Hector Corrada Bravo <hcorrada@gmail.com>
2014-11-10 12:31:37 -0500

    second alignment lecture

---
+++ homeworks/alignment.md
@@ -0,0 +1,21 @@
+---
+title: Alignment Exercise
+author: CMSC423 Fall 2014
+date: November 10, 2014 
+---
+
+Name(s):   
+UID(s):  
+
+**Question 1.** Calculate an optimal global alignment for strings `canon` and `gannon`
+with parameters: match 2, mismatch -1, gap -5. Show DP graph (don’t
+need to fill scores in completely), indicate path for resulting alignment, and write the final alignment.
+
+**Question 2.** (a) Would we get the same answer with mismatch penalty
+-10? (b) Would we get the same answer with mismatch penalty -100?
+(c) What is the smallest mismatch penalty that gives alignment in (b)?
+
+**Question 3.** Calculate an optimal local alignment for the same strings with the
+Same parameters as Problem 1. Show DP graph and final alignment.
+
+
--- src/rosalind/ex49_localalign/ex49.py
+++ src/rosalind/ex49_localalign/ex49.py
@@ -9,33 +9,47 @@ from Bio.SubsMat.MatrixInfo import pam250 as p250
 def get_score(x, y, mat):
   return mat[(x,y)] if (x,y) in mat else mat[(y,x)]
 
-# outputs the lcs alignment given
-# backtrack 
-def outputalign(backtrack, v, w, i, j, string1, string2):
+# outputs the local alignment given
+# backtrack starting at node (i,j)
+def outputalign(backtrack, v, w, i, j):
+    string1 = ''
+    string2 =''
+    
     # uses this construct to avoid
     # deep recursion limit
     while True:
         if backtrack[i,j] == 'st' or (i==0 and j==0):
+            # starting over or reached source
             return string1, string2
         if backtrack[i,j] == 'dn':
+            # took a down edge: gap in w
             i, string1, string2 = i-1, v[i-1] + string1, '-' + string2
         elif backtrack[i,j] == 'rt':
+            # took a right edge: gap in v
             j, string1, string2 = j-1, '-' + string1, w[j-1] + string2
         else:
+            # aligned, use a character from each string
             i, j, string1, string2 = i-1, j-1, v[i-1] + string1, w[j-1] + string2
 
 # computes local alignment
 # for strings v and w using dynamic programming
-# and gap penalty sigma
+# and gap penalty sigma (usually a negative number)
 def localalign(v, w, sigma):
     nv = len(v)
     nw = len(w)
 
     # initialize dp score and backtrack matrices
     s = np.zeros((nv+1,nw+1))
+
+    # backtrack will contain codes for the type of arrow:
+    # 'st': starting over (took the incoming edge from source node)
+    # 'dn': took a "down" arrow: a gap in w
+    # 'rt': took a "right" arrow: a gap in v
+    # 'di': took a "diagonal" arrow: align character from v and w
     backtrack = np.zeros((nv+1, nw+1), np.dtype('a2'))
 
-    allscores = [(0, (0,0))]
+    best_score = -float('inf')
+    best_node = (0,0)
     
     # fill in the dp matrices
     for i in xrange(0,nv+1):
@@ -43,27 +57,36 @@ def localalign(v, w, sigma):
             # use a heap to find max choice
             # use -score since heap keeps min in root
 
-            # start with the 0 choice to stop the alignment
+            # start with the 0 (incoming arrow from source node)
             choices = [(0, 'st')]
 
             if i > 0:
+                # gap in w
                 heapq.heappush(choices, (-(s[i-1,j] + sigma), 'dn'))
 
             if j > 0:
+                # gap in v
                 heapq.heappush(choices, (-(s[i,j-1] + sigma), 'rt'))
 
             if i>0 and j>0:
+                # align characters
                 score = get_score(v[i-1], w[j-1], p250)
                 heapq.heappush(choices, (-(s[i-1,j-1] + score), 'di'))
+                
             if len(choices) > 0:
                 choice = choices[0]
                 s[i,j] = -choice[0]
-                heapq.heappush(allscores, (-s[i,j], (i,j)))
                 backtrack[i,j] = choice[1]
-    choice = allscores[0]
-    score = -choice[0]
-    i,j = choice[1]
-    string1, string2 = outputalign(backtrack, v, w, i, j, '', '')
+
+                # update the best overall score, since we can start backtracking anywhere in graph
+                if s[i,j] > best_score:
+                    best_score = s[i,j]
+                    best_node = (i,j)
+
+    # start backtracking in best overall scoring node
+    score = best_score
+    i,j = best_node
+    string1, string2 = outputalign(backtrack, v, w, i, j)
     return int(score), string1, string2
 
 # read data
