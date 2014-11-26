b5573096a6d5501f0a788da7ed2190d28533af2b
Hector Corrada Bravo <hcorrada@gmail.com>
2014-11-26 07:34:52 -0500

    suffix array

---
+++ src/rosalind/ex54_suffixArrayConstruction/ex54.py
@@ -0,0 +1,24 @@
+import sys
+
+# construct suffix array from given text
+def make_suffix_array(text):
+    # setup list of indices
+    sa = [i for i in xrange(len(text))]
+
+    # sort the indices by corresponding suffixes
+    sa = sorted(sa, key=lambda i: text[i:])
+    return sa
+
+def readdat(filename):
+    with open(filename, 'r') as f:
+        return f.readline().strip()
+
+def main(filename):
+    text = readdat(filename)
+    sa = make_suffix_array(text)
+    print ", ".join(map(str,sa))
+    
+if __name__ == '__main__' and 'get_ipython' not in dir():
+    filename = sys.argv[1]
+    main(filename)
+    
