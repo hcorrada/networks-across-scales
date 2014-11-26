6ac0438ca624354f9a2ec4d6d61efdc59d015feb
Hector Corrada Bravo <hcorrada@gmail.com>
2014-11-26 16:00:58 -0500

    bwt construction

---
+++ src/rosalind/ex55_bwtConstruction/ex55.py
@@ -0,0 +1,31 @@
+import sys
+
+def get_bwt(text):
+    # rotate text so that last character of rotation is on the appropriate position
+    rotated_text = text[-1] + text[:-1]
+
+    # make tuples of character in bwt and the rotation it corresponds to
+    indexed_chars = zip(list(rotated_text), [i for i in xrange(len(text))])
+
+    # sort tuples by corresponding rotations
+    # the key function return the rotated text starting at appropriate index
+    sorted_chars = sorted(indexed_chars, key=lambda x: text[x[1]:] + text[:x[1]])
+
+    # extract the bwt characters after sorting rotations
+    bwt_chars, _ignore = zip(*sorted_chars)
+
+    # turn into a string and return
+    return ''.join(list(bwt_chars))
+
+def readdat(filename):
+    with open(filename, 'r') as f:
+        return f.readline().strip()
+
+def main(filename):
+    text = readdat(filename)
+    bwt = get_bwt(text)
+    print bwt
+
+if __name__ == '__main__' and 'get_ipython' not in dir():
+    filename = sys.argv[1]
+    main(filename)
