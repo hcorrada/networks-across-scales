b34505b2201845428d923bdc6a44f7ec205ff0a4
Hector Corrada Bravo <hcorrada@gmail.com>
2014-11-27 08:06:18 -0500

    checking edit distance over candidate substrings

--- src/rosalind/final14_approximateBWT/final14.py
+++ src/rosalind/final14_approximateBWT/final14.py
@@ -10,6 +10,45 @@ def readdat(filename):
         d = int(f.readline().strip())
         return text, patterns, d
 
+def make_seeds(pattern, d):
+    n = len(pattern)
+    k = n / (d + 1)
+    args = [iter(pattern)] * k
+    seeds = [''.join(x) for x in izip_longest(fillvalue='', *args)]
+    if len(seeds) > d+1:
+        last = seeds[-1]
+        seeds = seeds[:-1]
+        seeds[-1] += last
+    return seeds, k
+
+def find_approximate_candidates(pattern, bwt, sa, d):
+    seeds, k = make_seeds(pattern, d)
+    indices = []
+    for i in xrange(len(seeds)):
+        seed = seeds[i]
+        top, bottom = bwt.find_matches(seed)
+        if top == -1:
+            continue
+        candidates = sa.get_match_indices(top, bottom)
+        for index in candidates:
+            candidate = index - i*k
+            if candidate not in indices:
+                indices.append(index - i*k)
+    return sorted(indices)
+
+def within_edit_distance(x, y, d):
+    dist = 0
+    for i in xrange(len(x)):
+        dist += 1 if x[i] != y[i] else 0
+        if dist > d:
+            return False
+    return True
+
+def find_approximate_matches(pattern, bwt, sa, d, text):
+    candidates = find_approximate_candidates(pattern, bwt, sa, d)
+    candidates = filter(lambda x: within_edit_distance(pattern, text[x:], d), candidates)
+    return candidates
+
 def main(filename):
     k = 5
     c = 5
@@ -21,10 +60,8 @@ def main(filename):
     sa = PartialSuffixArray(text, k, bwt)
     
     indices = []
-    for pattern in patterns[:1]:
-        top, bottom = bwt.find_matches(pattern)
-        if top != -1:
-            indices += sa.get_match_indices(top, bottom)
+    for pattern in patterns:
+        indices += find_approximate_matches(pattern, bwt, sa, d, text)
     print ' '.join(map(str, sorted(indices)))
 
 if __name__ == '__main__' and 'get_ipython' not in dir():
