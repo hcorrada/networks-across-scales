23ff437a1ee98399705d2bfc777e579a2ceb9f8a
Hector Corrada Bravo <hcorrada@gmail.com>
2015-09-29 09:41:36 -0400

    testing done

--- src/rosalind/ex35_distortion/squared_error_distortion.py
+++ src/rosalind/ex35_distortion/squared_error_distortion.py
@@ -3,75 +3,66 @@ import re
 import heapq
 import math
 
-# compute euclidean distance between
+# compute squared euclidean distance between
 # vectors x and y of length m
 def point_to_point_distance(x, y, m):
-    res = 0
+    res = 0.
     for i in xrange(m):
         res += math.pow(x[i] - y[i], 2)
     return res
 
 # return the minimum euclidean distance
-# between point y and set of points x
+# between point y and set of k points centers
 # points are of dimension m
-def point_to_set_distance(y, x, m):
+def point_to_set_distance(y, centers, k, m):
     heap = []
-    for i in xrange(len(x)):
-        heapq.heappush(heap, point_to_point_distance(y, x[i], m))
+    for i in xrange(k):
+        heapq.heappush(heap, point_to_point_distance(y, centers[i], m))
     return heap[0]
 
 # compute squared error distortion
-# average distance between points in Data to closest center in X
-def squared_error_distortion(Data, X, ndata, nx, m):
+# average distance between points in data to closest center in centers
+def squared_error_distortion(data, centers, nrows, k, m):
     res = 0.
-    for i in xrange(ndata):
-        res += point_to_set_distance(Data[i], X, m)
-        
-    return res / ndata
+    for i in xrange(nrows):
+        res += point_to_set_distance(data[i], centers, k, m)
+
+    return res / nrows
 
 # read problem data from file
 # returns:
-#   Data: Data matrix 
-#   X: center matrix
-#   ndata: number of rows in Data
-#   nx: number of rows in X
-#   m: number of columns in Data and X
+#   data: Data matrix
+#   centers: center matrix
+#   nrows: number of rows in Data
+#   k: number of centers
+#   m: number of data dimensions
 def readdat(filename):
-    X = []
-    Data = []
-
-    ndata = 0
-    nx = 0
-    m = 0
-
-    reading_x = True
-    
     with open(filename, 'r') as f:
-        for string in f:
-            string = string.strip()
-            if re.match("^Dat", string) is not None:
-                reading_x = False
-                continue
+        k, m = map(int, f.readline().strip().split())
+        centers = []
+        for _ in xrange(k):
+            cur_center = map(float, f.readline().strip().split())
+            centers.append(cur_center)
 
-            vec = string.split()
-            if m == 0:
-                m = len(vec)
-            else:
-                assert(len(vec) == m)
+        # skip this line
+        _ = f.readline()
 
-            if reading_x:
-                X.append(map(float, vec))
-                nx += 1
-            else:
-                Data.append(map(float, vec))
-                ndata += 1        
-    return (Data, X, ndata, nx, m)
+        data = []
+        nrows = 0
+
+        # now read the remaining lines
+        for line in f:
+            nrows += 1
+            cur_point = map(float, line.strip().split())
+            data.append(cur_point)
+
+        return data, centers, nrows, k, m
 
 def main(filename):
-    Data, X, ndata, nx, m = readdat(filename)
-    dist = squared_error_distortion(Data, X, ndata, nx, m)
+    data, centers, nrows, k, m = readdat(filename)
+    dist = squared_error_distortion(data, centers, nrows, k, m)
     print "%.3f" % dist
-    
+
 if __name__ == '__main__':
     filename = sys.argv[1]
     main(filename)
