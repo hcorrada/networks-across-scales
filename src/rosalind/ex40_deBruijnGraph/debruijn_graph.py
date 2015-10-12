5fa2d0452098e757583e3a809526f2c95d5fe3d7
Hector Corrada Bravo <hcorrada@gmail.com>
2015-10-12 15:33:07 -0400

    comment

--- src/rosalind/ex40_deBruijnGraph/debruijn_graph.py
+++ src/rosalind/ex40_deBruijnGraph/debruijn_graph.py
@@ -1,5 +1,11 @@
 import sys
 
+# compute the kmer composition of string
+# input:
+#   k: k-mer length
+#   text: dna string
+# output:
+#   list of k-mers in text
 def kmer_composition(k, text):
     kmers = []
     for i in xrange(len(text) - k +1):
@@ -7,28 +13,49 @@ def kmer_composition(k, text):
     kmers.sort()
     return kmers
 
+# read input from file
+# expected format:
+#   k
+#   text
+# output:
+#   tuple k, text
 def readdat(filename):
     with open(filename, 'r') as f:
         k = int(f.readline().strip())
         test = f.readline().strip()
     return k, test
 
-def makeGraph(k, kmers):
+# make debruijn graph, i.e., k-mer overlap graph
+# input:
+#   kmers: list of kmers
+# output:
+#   dictionary:
+#       keys: k-1 mers (edge source)
+#       values: k-mers (edge targets)
+def makeGraph(kmers):
     graph = {}
-    for i in xrange(len(kmers)):
+    nkmers = len(kmers)
+    for i in xrange(nkmers):
         kmer = kmers[i]
         source = kmer[:-1]
         target = kmer[1:]
+
+        # if source is already a key in dictionary
         if source in graph:
+            # append target to target list
             graph[source].append(target)
         else:
+            # add to graph dictionary
             graph[source] = [target]
+    # sort by source and return
     return sorted(graph.items())
-    
+
 def main(filename):
     k, text = readdat(filename)
     kmers = kmer_composition(k, text)
-    graph = makeGraph(k, kmers)
+    graph = makeGraph(kmers)
+
+    # print out adjacency list for each node
     out = [x + " -> " + ",".join(y) for (x,y) in graph]
     print "\n".join(out)
 
