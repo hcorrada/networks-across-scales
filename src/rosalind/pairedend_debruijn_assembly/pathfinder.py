c21ccc4123cc8920bcacacbd1c83d41a43e06543
Hector Corrada Bravo <hcorrada@gmail.com>
2015-10-23 15:57:39 -0400

    cycle finding works for linear graph; need to reorder path

--- src/rosalind/pairedend_debruijn_assembly/pathfinder.py
+++ src/rosalind/pairedend_debruijn_assembly/pathfinder.py
@@ -1,5 +1,24 @@
 from path import Path
 from collections import defaultdict
+import itertools
+
+class ProbedIter:
+    def __init__(self, it):
+        self._it, self._next_it = itertools.tee(iter(it))
+        self._advance()
+
+    def _advance(self):
+        try:
+            self._lookahead = self._next_it.next()
+        except StopIteration:
+            self._lookahead = None
+
+    def next(self):
+        self._advance()
+        return self._it.next()
+
+    def has_more(self):
+        return self._lookahead is not None
 
 class CycleFinder:
     def __init__(self, g, k, d):
@@ -7,27 +26,40 @@ class CycleFinder:
         self._k = k
         self._d = d
 
-        self._available = dict([(node.label(), True) for node in self._graph])
-        self._target_iterators = dict([(node.label(), iter(node.target_labels())) for node in self._graph])
+        self._target_iterators = dict([(node.label(), ProbedIter(node.target_labels())) for node in self._graph])
         self._cycle_items = defaultdict(list)
+
+    def next_target(self, node):
+        it = self._target_iterators[node.label()]
+        if it.has_more():
+            label = it.next()
+            return self._graph[label]
+        else:
+            return None
+
+    def is_available(self, node):
+        it = self._target_iterators[node.label()]
+        return it.has_more()
 
     def make_a_cycle(self, start_node):
         path = Path(self._k, self._d)
-        path.append(start_node)
+
+        cur_node = start_node
+        while self.is_available(cur_node):
+            path.append(cur_node)
+            cur_node = self.next_target(cur_node)
         return path
 
     def find_cycle(self):
         cycle = Path(self._k, self._d)
 
         for node in self._graph:
-            if not self._available[node.label()]:
+            if not self.is_available(node):
                 continue
-
-            self._available[node.label()] = False
 
             print "next node:"
             print node
-            
+
             new_cycle = self.make_a_cycle(node)
             print new_cycle
             cycle = cycle.stitch(None, new_cycle)
