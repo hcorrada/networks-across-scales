b5b8e9332d5230c10109b2b7668fdaa032a34e6c
Hector Corrada Bravo <hcorrada@gmail.com>
2015-10-29 06:33:33 -0400

    test passeed

--- src/rosalind/overlap_align/overlap_align.py
+++ src/rosalind/overlap_align/overlap_align.py
@@ -2,12 +2,10 @@ import sys
 import numpy as np
 import heapq
 
-from Bio.SubsMat.MatrixInfo import blosum62 as bl62
-
 # wrapper to get score from
 # hash representing upper triangular matrix
 def get_score(x, y, mat):
-    out = 1 if x==y else -1
+    out = 1 if x==y else -2
     return out
 
 
@@ -16,7 +14,7 @@ def get_score(x, y, mat):
 def outputalign(backtrack, v, w, i, j):
     string1 = ''
     string2 =''
-    
+
     # uses this construct to avoid
     # deep recursion limit
     while True:
@@ -24,7 +22,7 @@ def outputalign(backtrack, v, w, i, j):
         # print string1, string2, i, j
         # HERE WAS THE TYPO (WE WROTE i==0 IN CLASS)
         if j==0:
-            # starting over or reached source
+            # reached source
             return string1, string2
         if backtrack[i,j] == 'dn':
             # took a down edge: gap in w
@@ -35,49 +33,51 @@ def outputalign(backtrack, v, w, i, j):
         else:
             # aligned, use a character from each string
             i, j, string1, string2 = i-1, j-1, v[i-1] + string1, w[j-1] + string2
-            
+
 # computes global alignment
 # for strings v and w using dynamic programming
 # and gap penalty sigma
-def globalalign(v, w, sigma):
+def overlap_align(v, w):
+    sigma = -2
+
     nv = len(v)
     nw = len(w)
 
     # initialize dp score and backtrack matrices
     s = np.zeros((nv+1,nw+1))
     backtrack = np.zeros((nv+1, nw+1), np.dtype('a2'))
-    
+
     # initialize dp table
-    for i in xrange(0,nv+1):
+    for i in xrange(0, nv+1):
         s[i,0] = 0
     for j in xrange(0, nw+1):
         s[0,j] = j * sigma
 
-    best_score = s[0,nw]
-    best_node = (0,nw)
+    best_score = s[nv,0]
+    best_node = (nv, 0)
 
     # fill in the dp matrices
-    for i in xrange(1,nv+1):
-        for j in xrange(1, nw+1):
+    for j in xrange(1, nw+1):
+        for i in xrange(1, nv+1):
             # use a heap to find max choice
             choices = []
 
             # use -score since heap keeps min in root
-            heapq.heappush(choices, (-(s[i-1,j] + sigma), 'dn'))
+            heapq.heappush(choices, (-(s[i-1, j] + sigma), 'dn'))
 
-            heapq.heappush(choices, (-(s[i,j-1] + sigma), 'rt'))
+            heapq.heappush(choices, (-(s[i, j-1] + sigma), 'rt'))
 
-            score = get_score(v[i-1], w[j-1], bl62)
+            score = get_score(v[i-1], w[j-1], None)
             heapq.heappush(choices, (-(s[i-1,j-1] + score), 'di'))
-            
+
             if len(choices) > 0:
                 choice = choices[0]
                 s[i,j] = -choice[0]
                 backtrack[i,j] = choice[1]
-        
-        if s[i,nw] > best_score:
-            best_score = s[i,nw]
-            best_node = (i,nw)
+
+        if s[nv, j] > best_score:
+            best_score = s[nv, j]
+            best_node = (nv, j)
 
     return int(best_score), backtrack, best_node
 
@@ -90,15 +90,14 @@ def readdat(filename):
 
 def check_solution(v,va,w,wa,score):
     assert len(va) == len(wa)
-    assert w == wa.replace('-','')
     score2 = 0
     for i in xrange(len(va)):
-        score2 += 1 if va[i] == wa[i] else -1
+        score2 += 1 if va[i] == wa[i] else -2
     assert score2 == score
-    
+
 def main(filename):
     v,w  = readdat(filename)
-    score, backtrack, (i,j) = globalalign(v, w, -1)
+    score, backtrack, (i,j) = overlap_align(v, w)
     va, wa = outputalign(backtrack, v, w, i, j)
     check_solution(v,va,w,wa,score)
     print score
