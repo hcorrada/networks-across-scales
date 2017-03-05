d69f77d8647356817113b745773cc5be07ea6ab3
hcorrada <hcorrada@gmail.com>
2017-03-05 09:14:28 -0500

    update clustering material

--- materials/lectures/clustering/diauxic.R
+++ materials/lectures/clustering/diauxic.R
@@ -20,10 +20,19 @@ o <- order(time)
 x <- exprs(eset)[,o]
 time <- time[o]
 
-# remove genes with any NAs
+# remove genes with any NAs (missing data)
 hasNas <- rowSums(is.na(x)) > 0
 x <- x[!hasNas,]
 geneNames <- featureData(eset)$ORF[!hasNas]
+
+# make a function to plot genes
+plotgene <- function(x, ...) {
+  matplot(time, t(x),type="b", lwd=1.6, pch=19, ylab="expression", ...)
+}
+
+# plot 100 random genes in data
+indices <- sample(nrow(x), 100)
+plotgene(x[indices,])
 
 # keep 500 genes with largest variance
 geneSds <- matrixStats::rowSds(x)
@@ -31,34 +40,9 @@ o <- order(-geneSds)
 x <- x[o[1:500],]
 geneNames <- geneNames[o[1:500]]
 
-# center and scale gene data
-geneMeans <- rowMeans(x)
-geneSds <- matrixStats::rowSds(x)
-
-sx <- x
-for (i in seq(len=nrow(x))) {
-    sx[i,] <- (x[i,] - geneMeans[i]) / geneSds[i]
-}
-
-# plot the first gene
-matplot(time, sx[1,], type="b", lwd=1.4,pch=19,ylab="expression",main=geneNames[1])
-
-# plot the first ten genes
-matplot(time, t(sx[1:10,]),type="b", lwd=1.4,pch=19,ylab="expression")
-
-# plot the first 100 genes
-matplot(time, t(sx[1:100,]),type="b", lwd=1.4,pch=19,ylab="expression")
-
-# make a function to plot genes
-plotgene=function(x, ...) {
-  matplot(time, t(x),type="b", lwd=1.4,pch=19,ylab="expression", ...)
-}
-
-# let's illustrate furtherst first traversal
-
 # use the first gene as first center
-center1 <- sx[1,]
-plotgene(rbind(center1), main="center 1")
+center1 <- x[1,,drop=FALSE]
+plotgene(center1, main="center 1")
 
 # this function computes distance between two points
 get_distance <- function(x,y) {
@@ -66,28 +50,30 @@ get_distance <- function(x,y) {
 }
 
 # compute the distance between each point and first center
-center1Distance <- apply(sx, 1, function(x) get_distance(x, center1))
+center1Distance <- apply(x, 1, function(x) get_distance(x, center1))
+similar_gene <- order(center1Distance)[2]
+plotgene(rbind(center1, x[similar_gene,]))
 
 # choose the gene that's furthest away as second center
 center2Index <- which.max(center1Distance)
-center2 <- sx[center2Index,]
+center2 <- x[center2Index,,drop=FALSE]
 
 plotgene(rbind(center1, center2), main="center 2")
 
 # compute distance between each gene and second center
-center2Distance <- apply(sx, 1, function(x) get_distance(x, center2))
+center2Distance <- apply(x, 1, function(x) get_distance(x, center2))
 
 # is center1 or center2 closest center
 minDistance <- apply(cbind(center1Distance, center2Distance),1,min)
 
 # choose the gene that's furthest away from it's closest center as third center
 center3Index <- which.max(minDistance)
-center3 <- sx[center3Index,]
+center3 <- x[center3Index,]
 
 plotgene(rbind(center1, center2, center3), main="center 3")
 
 # compute distance to third center
-center3Distance <- apply(sx, 1, function(x) get_distance(x, center3))
+center3Distance <- apply(x, 1, function(x) get_distance(x, center3))
 distances <- cbind(center1Distance, center2Distance, center3Distance)
 
 # assign each gene to cluster based on closest center
@@ -98,13 +84,13 @@ table(clusterIndex)
 for (k in seq(len=3)) {
   indx <- which(clusterIndex == k)
   indx <- indx[order(minDistance[indx])][1:50]
-  plotgene(sx[indx,], main=paste("cluster", k))
+  plotgene(x[indx,], main=paste("cluster", k))
 }
 
-plotgene(rbind(colMeans(sx[clusterIndex==1,])))
+plotgene(rbind(colMeans(x[clusterIndex==1,])))
 
 # run k-means to find six centers
-res=kmeans(sx, centers=6)
+res <- kmeans(x, centers=6)
 
 # plot the 6 "center" genes
 plotgene(res$centers, main="kmeans centers")
@@ -112,7 +98,7 @@ plotgene(res$centers, main="kmeans centers")
 # plot the 6 gene clusters
 for (k in seq(len=6)) {
   ind <- which(res$cluster == k)
-  plotgene(sx[ind,], main=paste("center", k))
+  plotgene(x[ind,], main=paste("center", k))
 }
 
 # get gene names in first cluster
@@ -136,6 +122,7 @@ hist(heights, nc=20)
 rug(heights)
 
 # mean height is not a good estimate 
+# I'm off by 3.5 inches on average
 my_guess <- mean(heights)
 abline(v=my_guess,lwd=2)
 mean(sqrt((heights-my_guess)^2))
@@ -164,7 +151,7 @@ z <- exp(-distances) / rowSums(exp(-distances))
 plot(heights, z[,1],pch=19,col="blue")
 points(heights, z[,2], pch=19, col="red")
 
-# let's guess again but weigh by our probability that each subject is male or
+# let's guess means again but weigh by our probability that each subject is male or
 # female (based on our current guess)
 # this is called the M-step in the EM algorithm
 weighted_guesses <- sapply(1:2, function(i) sum(z[,i] * heights) / sum(z[,i]))
@@ -184,6 +171,14 @@ abline(v=weighted_guesses, col=c("blue","red"), lwd=2, lty=3)
 
 truth=rep(2:1, len=100)
 # now we have much better estimates
+# the best we could ever do is be wrong by 2.67 inches on average
+# with one iteration of EM we are wrong 2.78 inches on average
 mean(sqrt((heights-weighted_guesses[truth])^2))
 mean(sqrt((heights-c(mu2,mu1)[truth])^2))
 
+library(mclust)
+em_res <- Mclust(x, G=6, modelNames="EII")
+plotgene(t(em_res$parameters$mean), main="EM centers")
+
+# plot the 6 "center" genes
+plotgene(res$centers, main="kmeans centers")
