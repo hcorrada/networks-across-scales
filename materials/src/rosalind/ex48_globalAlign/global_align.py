fe9e773d20eab0b656161194754723bd54442d4b
hcorrada <hcorrada@gmail.com>
2017-04-13 12:33:06 -0400

    update code slightly

--- materials/src/rosalind/ex48_globalAlign/global_align.py
+++ materials/src/rosalind/ex48_globalAlign/global_align.py
@@ -10,74 +10,79 @@ def get_score(x, y, mat):
   return mat[(x,y)] if (x,y) in mat else mat[(y,x)]
 
 # outputs the alignment given
-# backtrack matrix, input strings and node in table
-# to start from
-def output_align(backtrack, v, w, i, j):
-    v_alignment = ''
-    w_alignment = ''
+# backtrack matrix, input strings and position in table
+# to start backtrack from
+def output_align(backtrack, a, b, i, j):
+    a_alignment = ''
+    b_alignment = ''
     
     # uses this construct to avoid
     # deep recursion limit
     while True:
         if i==0 and j==0:
-            return v_alignment, w_alignment
+            return a_alignment, b_alignment
         if backtrack[i,j] == 'dn':
-            i, v_alignment, w_alignment = i-1, v[i-1] + v_alignment, '-' + w_alignment
+            i, a_alignment, b_alignment = i-1, a[i-1] + a_alignment, '-' + b_alignment
         elif backtrack[i,j] == 'rt':
-            j, v_alignment, w_alignment = j-1, '-' + v_alignment, w[j-1] + w_alignment
+            j, a_alignment, b_alignment = j-1, '-' + a_alignment, b[j-1] + b_alignment
         else:
-            i, j, v_alignment, w_alignment = i-1, j-1, v[i-1] + v_alignment, w[j-1] + w_alignment
+            i, j, a_alignment, b_alignment = i-1, j-1, a[i-1] + a_alignment, b[j-1] + b_alignment
 
 # computes global alignment
 # for strings v and w using dynamic programming
 # and gap penalty sigma
-def global_align(v, w, sigma):
-    nv = len(v)
-    nw = len(w)
+def global_align(a, b, sigma):
+    n = len(a)
+    m = len(b)
 
     # initialize dp score and backtrack matrices
-    s = np.zeros((nv+1,nw+1))
-    backtrack = np.zeros((nv+1, nw+1), np.dtype('a2'))
+    s = np.zeros((n+1,m+1))
+    backtrack = np.zeros((n+1, m+1), np.dtype('a2'))
+
+    # initialize dp table
+    for i in xrange(0, n+1):
+        s[i,0] = -sigma * i
+        backtrack[i,0] = 'dn'
+
+    for j in xrange(0, m+1):
+        s[0,j] = -sigma * j
+        backtrack[0,j] = 'rt'
 
     # fill in the dp matrices
-    for i in xrange(0,nv+1):
-        for j in xrange(0, nw+1):
+    for i in xrange(1, n+1):
+        for j in xrange(1, m+1):
             # use a heap to find max choice
             choices = []
 
             # use -score since heap keeps min in root
-            if i > 0:
-                heapq.heappush(choices, (-(s[i-1,j] + sigma), 'dn'))
+            heapq.heappush(choices, (-(s[i-1,j] - sigma), 'dn'))
+            heapq.heappush(choices, (-(s[i,j-1] - sigma), 'rt'))
 
-            if j > 0:
-                heapq.heappush(choices, (-(s[i,j-1] + sigma), 'rt'))
+            score = get_score(a[i-1], b[j-1], bl62)
+            heapq.heappush(choices, (-(s[i-1,j-1] + score), 'di'))
 
-            if i>0 and j>0:
-                score = get_score(v[i-1], w[j-1], bl62)
-                heapq.heappush(choices, (-(s[i-1,j-1] + score), 'di'))
+            choice = choices[0]
+            s[i,j] = -choice[0]
+            backtrack[i,j] = choice[1]
 
-            if len(choices) > 0:
-                choice = choices[0]
-                s[i,j] = -choice[0]
-                backtrack[i,j] = choice[1]
-
-    score = s[nv, nw]
+    score = s[n, m]
     return int(score), backtrack
 
 # read data
 def readdat(filename):
     with open(filename, 'r') as f:
-        v = f.readline().strip()
-        w = f.readline().strip()
-    return v,w
+        a = f.readline().strip()
+        b = f.readline().strip()
+    return a, b
 
 def main(filename):
-    v,w  = readdat(filename)
-    score, backtrack = global_align(v, w, -5)
-    v_alignment, w_alignment = output_align(backtrack, v, w, len(v), len(w))
-    print score
-    print v_alignment
-    print w_alignment
+  sigma = 5 # given in problem definition
+  a, b  = readdat(filename)
+  score, backtrack = global_align(a, b, sigma)
+  a_alignment, b_alignment = output_align(backtrack, a, b, len(a), len(b))
+  print score
+  print a_alignment
+  print b_alignment
 
 if __name__ == '__main__' and 'get_ipython' not in dir():
     filename = sys.argv[1]
