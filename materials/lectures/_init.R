bd3809840b7966bbd0daab9ca3f20dc2dd5a4e3d
hcorrada <hcorrada@gmail.com>
2017-08-28 19:51:51 -0400

    setup slide template; start course intro slides

--- materials/lectures/_init.R
+++ materials/lectures/_init.R
@@ -4,12 +4,12 @@ title <- args[1]
 
 dir.create(title)
 dir.create(file.path(title, "img"))
-file.copy(file.path("img", "epiviz.png"), file.path(title, "img", "epiviz.png"))
+file.copy(file.path("img", "head.png"), file.path(title, "img", "head.png"))
 file.copy(file.path("img", "logo.png"), file.path(title, "img", "logo.png"))
 file.copy("template.rmd", file.path(title, "index.rmd"))
 file.copy("custom.css", file.path(title, "custom.css"))
 file.copy("custom.html", file.path(title, "custom.html"))
-file.symlink(path.expand("../notes/images"), title)
+file.symlink(path.expand("../images"))
 
 #cmd <- paste("cat", "template.rmd", rmd, ">", file.path(title, "index.rmd"))
 #system(cmd)
